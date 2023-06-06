0b6443ad5867a617a4d11e56433ff956c54c3092
imad <imad.oulasri@iu-study.org>
2023-06-06 13:54:20 +0100

    print tebahpla

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a')-1, -1):
-    if i % 2 == 0:
-        print(chr(i), end='')
-    else:
-        print(chr(i - 32), end='')
+    print(chr(i) if i % 2 == 0 else chr(i - 32), end='')
