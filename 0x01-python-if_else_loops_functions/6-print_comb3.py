1e13d1fd595eaddcf56177d4dc8e725ea4cf5659
imad <imad.oulasri@iu-study.org>
2023-06-06 12:43:27 +0100

    print comb2

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,2 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-    a = i + 1
-    while a <= 9:
-        if a == 9 and i == 8:
-            print(89)
-        else:
-            print('{}{}'.format(i, a), end=', ')
-        a += 1
+print(', '.join('{}{}'.format(i, j) for i in range(9) for j in range(i+1, 10)))
