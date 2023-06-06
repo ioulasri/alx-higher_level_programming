329fb8053779cb30d85febdbfc364d3dc01e2e37
imad <imad.oulasri@iu-study.org>
2023-06-06 13:01:17 +0100

    convert to upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) >= ord('A') and ord(c) <= ord('Z'):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
         return 1
     return 0
+
+def uppercase(str):
+    result = ""
+    for i in str:
+        if islower(i):
+            result += chr(ord(i) - 32)
+        else:
+            result += i
+    print('{}'.format(result))
