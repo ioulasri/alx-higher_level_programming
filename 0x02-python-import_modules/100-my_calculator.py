521a0a9110819e4e21a74a75943e31a2bf1f1172
imad <imad.oulasri@iu-study.org>
2023-06-08 13:52:54 +0100

    My own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,23 +5,18 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import mul, sub, add, div
 
-    op = '+*/-'
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    if sys.argv[2] not in op:
-        print("Unknown operator. Available operators: +, -, * and / ")
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
+
+    dic = {"+": add, "-": sub, "*": mul, "/": div}
+
+    if op not in dic:
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    a = sys.argv[1]
-    b = sys.argv[3]
-    dict = {'*': mul(int(a), int(b)),
-            '-': sub(int(a), int(b)),
-            '+': add(int(a), int(b)),
-            '/': div(int(a), int(b)),
-            }
-
-    for key, value in dict.items():
-        if key == sys.argv[2]:
-            print("{} {} {} = {}".format(a, key, b, value))
+    print("{} {} {} = {}".format(a, op, b, dic[op](a, b)))
