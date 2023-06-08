15e4b5ee62c985e1ace86772c58420eadfbfe222
imad <imad.oulasri@iu-study.org>
2023-06-08 13:13:24 +0100

    add all the arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Infinite add all the arguments"""
+    import sys
+
+    sum = 0
+    if len(sys.argv) == 1:
+        print(sum)
+    else:
+        for i in range(1, len(sys.argv)):
+            sum += int(sys.argv[i])
+        print(sum)
+
+
