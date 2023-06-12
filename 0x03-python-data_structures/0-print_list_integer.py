3254186f5097a778c56b7780a766363dd7dd8594
imad <imad.oulasri@iu-study.org>
2023-06-13 00:14:01 +0100

    fixing errors

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """printing items in list"""
-    def print_list_integer(my_list=[]):
-        for i in my_list:
-            print("{:d}".format(i))
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print("{:d}".format(i))
