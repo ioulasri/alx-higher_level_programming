a43d4e0d4cd56f8970f72575b2f351bf373f7ec1
imad <imad.oulasri@iu-study.org>
2023-06-14 11:57:02 +0100

    returnin key with best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 def best_score(a_dictionary):
     key = None
     best = -1
-    if (a_dictionary == None):
+    if (a_dictionary is None):
         return key
     for k, v in a_dictionary.items():
         if v > best:
