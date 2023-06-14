362a5013ea67765c4ff522491b0716af00a5a32e
imad <imad.oulasri@iu-study.org>
2023-06-14 23:29:03 +0100

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if not roman_string or roman_string == "":
+    if not isinstance(roman_string, str):
         return 0
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
     prev_value = 0
     for char in roman_string:
