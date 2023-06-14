40cebacbb86f6a5caa124c512fe52ee27e3d505e
imad <imad.oulasri@iu-study.org>
2023-06-14 12:40:35 +0100

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -16,6 +16,16 @@ def roman_to_int(roman_string):
                 sum += 4
                 i += 1
                 o = 0
+        elif roman_string[i] == 'X' and i + 1 < len(roman_string):
+            if roman_string[i + 1] == 'L':
+                sum += 40
+                i += 1
+                o = 0
+        elif roman_string[i] == 'C' and i + 1 < len(roman_string):
+            if roman_string[i + 1] == 'D':
+                sum += 400
+                i += 1
+                o = 0
         if o:
             sum += dictionary[roman_string[i]]
         i += 1
