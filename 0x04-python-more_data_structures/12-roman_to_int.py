cb7fbada5e08938d034984cff1c3dd42f3dd49d2
imad <imad.oulasri@iu-study.org>
2023-06-14 23:27:10 +0100

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if not roman_string or roman_string == "":
         return 0
     total = 0
     prev_value = 0
     for char in roman_string:
-        if char in dictionary:
-            current_value = dictionary[char]
+        if char in dic:
+            current_value = dic[char]
             if current_value > prev_value:
                 total += current_value - 2 * prev_value
             else:
@@ -16,4 +16,3 @@ def roman_to_int(roman_string):
         else:
             return 0
     return total
-
