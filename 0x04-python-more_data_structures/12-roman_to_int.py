35d1edd2dfae4f27a479ffd1d17531fd25754159
imad <imad.oulasri@iu-study.org>
2023-06-14 23:25:11 +0100

    roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     dictionary = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if not roman_string:
+    if not roman_string or roman_string == "":
         return 0
     total = 0
     prev_value = 0
