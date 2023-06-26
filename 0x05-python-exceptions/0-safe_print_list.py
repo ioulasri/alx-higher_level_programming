cd4ea97301232c15e4a6603cea1c27883b83808c
imad <imad.oulasri@iu-study.org>
2023-06-26 13:46:33 +0100

    printing a list safely

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    """print x elements of a list.
+
+    Args:
+        my_list (list): list of all elements.
+        x (int): the number of time you need to iterate over the list
+    Return:
+        The number of elements printed.
+    """
     try:
         for i in range(0, x):
             print(my_list[i], end="")
