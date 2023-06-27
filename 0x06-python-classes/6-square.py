a71fab6d497a44319c4ef166672fd73097ec7704
imad <imad.oulasri@iu-study.org>
2023-06-27 14:29:24 +0100

    updating the file

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -45,6 +45,7 @@ class Square():
     def my_print(self):
         if self.__size == 0:
             print()
+        [print() for i in range(0, self.__position[1])]
         for i in range(self.__size):
             print(" " * self.__position[0], end="")
             for j in range(self.__size):
