798a9c59bb9ac9ca00c7e6b54b00c8ae0b1c1fd7
imad <imad.oulasri@iu-study.org>
2023-07-03 15:04:34 +0100

    printing the rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -4,17 +4,22 @@
 
 
 class Rectangle():
-    """constructor"""
+    """ inside class rectangle"""
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Get the width of the Rectangle."""
+
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Set the width of the Rectangle."""
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -23,10 +28,14 @@ class Rectangle():
 
     @property
     def height(self):
+        """Get the height of the Rectangle."""
+
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Set the height of the Rectangle."""
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -34,9 +43,13 @@ class Rectangle():
         self.__height = value
 
     def area(self):
+        """Return the area of the Rectangle."""
+
         return self.__height * self.__width
 
     def perimeter(self):
+        """Return the perimeter of the Rectangle."""
+
         if (not self.__height or not self.__width):
             return 0
         return 2 * (self.__width + self.__height)
@@ -46,11 +59,6 @@ class Rectangle():
 
         Represents the rectangle with the # character.
         """
-        if self.__width == 0 or self.__height == 0:
-            return ("")
-        rect = []
-        for i in range(self.__height):
-            for j in range(self.__width):
-                rect.append('#')
-            rect.append('\n')
-        return ''.join(rect)
+        if (not self.__height or not self.__width):
+            return ""
+        return "\n".join(["#" * self.__width] * self.__height)
