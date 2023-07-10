d2bced45fff7b68a0ff081a766d49edb4434baeb
imad <imad.oulasri@iu-study.org>
2023-07-10 11:56:10 +0100

    print list obj

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Defines a function that returns the list of available attributes and methods of an object"""
 
 def lookup(obj):
     """Returns the list of available attributes and methods of an object"""
