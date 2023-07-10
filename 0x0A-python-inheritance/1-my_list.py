f27aab0e87077221df72461fa218ad142dc2d3d9
imad <imad.oulasri@iu-study.org>
2023-07-10 12:06:36 +0100

    prints a list sorted

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,6 +5,10 @@
 class Mylist(list):
     """ Mylist class """
 
+    def __init__(self):
+        """ Constructor method """
+        super().__init__()
+
     def print_sorted(self):
         """ Prints the list, but sorted (ascending sort) """
         print(sorted(self))
