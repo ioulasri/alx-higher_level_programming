586e7a9fce71bddde7f3f8c7630819ca14307505
imad <imad.oulasri@iu-study.org>
2023-07-11 14:30:48 +0100

    adding items to a json file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""Module for add_item to json"""
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
     try:
-        my_list = load_from_json_file('add_item.json')
+        m_list = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        my_list = []
-    my_list.extend(sys.argv[1:])
-    save_to_json_file(my_list, 'add_item.json')
+        m_list = []
+    m_list.extend(sys.argv[1:])
+    save_to_json_file(m_list, "add_item.json")
