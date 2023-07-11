c3f92b23cae14240a90a86ab060611a1f38edaf8
imad <imad.oulasri@iu-study.org>
2023-07-11 14:26:18 +0100

    adding items to a json file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for add_item to json"""
+import sys
+
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+def main():
+    """Main function"""
+    try:
+        my_list = load_from_json_file('add_item.json')
+    except:
+        my_list = []
+    for i in range(1, len(sys.argv)):
+        my_list.append(sys.argv[i])
+    save_to_json_file(my_list, 'add_item.json')
